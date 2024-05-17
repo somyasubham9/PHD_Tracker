670e6e9c987ead1c182a8c58bc1a407a1d914eb7
Subham Rath <81670473+SUBHAMX11@users.noreply.github.com>
2024-05-17 13:23:58 +0530

    folder

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import "./App.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Form1ALayout from "./app/Page/Form1Page/Form1ALayout";
 
 
 
@@ -12,7 +13,7 @@ function App() {
     <Router>
       <main className="pt-12">
         <Routes>
-          <Route path="/" element={<HomeLayout />} />
+          <Route path="/" element={<Form1ALayout />} />
         </Routes>
       </main>
     </Router>
---
+++ src/app/Page/Form1Page/Form1ALayout.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Form1ALayout() {
+  return (
+    <div>Form1ALayout</div>
+  )
+}
+
+export default Form1ALayout

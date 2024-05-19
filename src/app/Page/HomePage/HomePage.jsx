78d78780e9d32ba0c566cc987bf61cc518c1d158
Subham Rath <81670473+SUBHAMX11@users.noreply.github.com>
2024-05-19 12:50:41 +0530

    navbar changes

--- src/app/Page/HomePage/HomePage.jsx
+++ src/app/Page/HomePage/HomePage.jsx
@@ -1,8 +1,10 @@
 import React from "react";
+import SocialProfile from "../../Components/Profile/HeaderComponent";
 
 const HomePage = () => {
   return (
     <>
+      <SocialProfile/>
     </>
   );
 };

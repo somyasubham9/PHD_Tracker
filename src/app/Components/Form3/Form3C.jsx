27284a1d1ac523008980dfdcd7f0f1c60fa63f31
Somya Subham Dash <workforssd@gmail.com>
2024-05-21 02:06:15 +0530

    upto 4A

--- src/app/Components/Form3/Form3C.jsx
+++ src/app/Components/Form3/Form3C.jsx
@@ -1,7 +1,12 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useForm3CSubmitMutation } from '../../Services/formService';
+import { useSelector } from "react-redux";
+import axios from "axios";
 
 const Form3C = () => {
+  const initialState = useSelector((state) => state.user);
+  const [isSubmitted, setIsSubmitted] = useState(false);
+
   const [scholarName, setScholarName] = useState('');
   const [rollNo, setRollNo] = useState('');
   const [branch, setBranch] = useState('');
@@ -18,6 +23,42 @@ const Form3C = () => {
     setCommitteeMembers(updatedMembers);
   };
 
+  useEffect(()=>{
+    const getForm3CData = async () => {
+      const token = sessionStorage.getItem("access");
+      if (!token) {
+        console.error("No access token found in session storage");
+        return;
+      }
+  
+      try {
+        console.log(initialState.userId);
+        const res = await axios.get(
+          `http://127.0.0.1:8000/api/form3C/user/${initialState.userId}/`,
+          {
+            headers: {
+              Authorization: `Bearer ${token}`,
+            },
+          }
+        );
+  
+        const data = res.data.data;
+        setScholarName(data.name);
+        setRollNo(data.rollno);
+        setBranch(data.branch);
+        setDateOfSeminar(data.date_of_seminar);
+        setTopicOfTheTalk(data.topic_of_talk);
+        setProgress(data.progress);
+        setCommitteeMembers(data.committee)
+  
+        setIsSubmitted(true);
+      } catch (error) {
+        // console.error('Error fetching data:', error.response ? error.response.data : error.message);
+      }
+    };
+    getForm3CData();
+  },[initialState.userId,isSubmitted])
+
   const handleSubmit = async (event) => {
     event.preventDefault();
     const formData = {
@@ -33,6 +74,7 @@ const Form3C = () => {
     try {
       const response = await form3cSubmit(formData);
       console.log(response);
+      setIsSubmitted(true);
     } catch (error) {
       console.error(error);
       alert("Failed to submit form.");
@@ -48,38 +90,38 @@ const Form3C = () => {
         
         <div>
           <label htmlFor="scholarName" className="block text-sm font-medium text-gray-700">Name of the Scholar:</label>
-          <input type="text" id="scholarName" value={scholarName} onChange={e => setScholarName(e.target.value)}
+          <input readOnly={isSubmitted} type="text" id="scholarName" value={scholarName} onChange={e => setScholarName(e.target.value)}
                  className="mt-1 w-full p-2 border border-gray-300 rounded-md shadow-sm"/>
         </div>
 
         <div className='flex w-full gap-8'>
           <div className='w-1/2'>
             <label htmlFor="rollNo" className="block text-sm font-medium text-gray-700">Roll No.:</label>
-            <input type="text" id="rollNo" value={rollNo} onChange={e => setRollNo(e.target.value)}
+            <input readOnly={isSubmitted} type="text" id="rollNo" value={rollNo} onChange={e => setRollNo(e.target.value)}
                    className="mt-1 w-full p-2 border border-gray-300 rounded-md shadow-sm"/>
           </div>
           <div className='w-1/2'>
             <label htmlFor="branch" className="block text-sm font-medium text-gray-700">Branch:</label>
-            <input type="text" id="branch" value={branch} onChange={e => setBranch(e.target.value)}
+            <input readOnly={isSubmitted} type="text" id="branch" value={branch} onChange={e => setBranch(e.target.value)}
                    className="mt-1 w-full p-2 border border-gray-300 rounded-md shadow-sm"/>
           </div>
         </div>
 
         <div>
           <label htmlFor="dateOfSeminar" className="block text-sm font-medium text-gray-700">Date of the Seminar:</label>
-          <input type="date" id="dateOfSeminar" value={dateOfSeminar} onChange={e => setDateOfSeminar(e.target.value)}
+          <input readOnly={isSubmitted} type="date" id="dateOfSeminar" value={dateOfSeminar} onChange={e => setDateOfSeminar(e.target.value)}
                  className="mt-1 w-full p-2 border border-gray-300 rounded-md shadow-sm"/>
         </div>
 
         <div>
           <label htmlFor="topicOfTheTalk" className="block text-sm font-medium text-gray-700">Topic of the talk:</label>
-          <input type="text" id="topicOfTheTalk" value={topicOfTheTalk} onChange={e => setTopicOfTheTalk(e.target.value)}
+          <input readOnly={isSubmitted} type="text" id="topicOfTheTalk" value={topicOfTheTalk} onChange={e => setTopicOfTheTalk(e.target.value)}
                  className="mt-1 w-full p-2 border border-gray-300 rounded-md shadow-sm"/>
         </div>
 
         <div>
           <label htmlFor="progress" className="block text-sm font-medium text-gray-700">Progress (Satisfactory / Unsatisfactory):</label>
-          <select id="progress" value={progress} onChange={e => setProgress(e.target.value)}
+          <select disabled={isSubmitted} id="progress" value={progress} onChange={e => setProgress(e.target.value)}
                   className="mt-1 w-full p-2 border border-gray-300 rounded-md shadow-sm">
             <option value="">Select Progress</option>
             <option value="satisfactory">Satisfactory</option>
@@ -91,20 +133,25 @@ const Form3C = () => {
           <h3 className="text-lg font-medium text-gray-700">Signature of Doctoral Scrutiny Committee Members:</h3>
           {committeeMembers.map((member, index) => (
             <div key={index} className="grid grid-cols-3 gap-4 mb-2">
-              <input type="text" placeholder="Name" value={member.name} onChange={(e) => handleMemberChange(index, 'name', e.target.value)}
+              <input readOnly={isSubmitted} type="text" placeholder="Name" value={member.name} onChange={(e) => handleMemberChange(index, 'name', e.target.value)}
                      className="col-span-1 p-2 border border-gray-300 rounded-md shadow-sm"/>
-              <input type="text" placeholder="Remarks" value={member.remarks} onChange={(e) => handleMemberChange(index, 'remarks', e.target.value)}
+              <input readOnly={isSubmitted} type="text" placeholder="Remarks" value={member.remarks} onChange={(e) => handleMemberChange(index, 'remarks', e.target.value)}
                      className="col-span-1 p-2 border border-gray-300 rounded-md shadow-sm"/>
-              <input type="text" placeholder="Signature" value={member.signature} onChange={(e) => handleMemberChange(index, 'signature', e.target.value)}
+              <input readOnly={isSubmitted} type="text" placeholder="Signature" value={member.signature} onChange={(e) => handleMemberChange(index, 'signature', e.target.value)}
                      className="col-span-1 p-2 border border-gray-300 rounded-md shadow-sm"/>
             </div>
           ))}
         </div>
 
         <div>
-          <button type="submit" className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline">
-            Submit
-          </button>
+        {!isSubmitted && (
+            <button
+              type="submit"
+              className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+            >
+              Submit
+            </button>
+          )}
         </div>
       </form>
     </div>
